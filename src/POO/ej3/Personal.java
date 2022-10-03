6b06a6622ef708779b222fd4c140299bfa4b5315
lihuen <lcarranza@fi.uba.ar>
2022-10-03 17:16:03 -0300

    crear historial en usuario

--- src/POO/ej3/Personal.java
+++ src/POO/ej3/Personal.java
@@ -1,5 +1,15 @@
 package POO.ej3;
 
-public class Personal {
+public class Personal extends Usuario{
+
+        private int rango; 
+
+        public Personal(String nombre2, String password, int rango2) {
+                super(nombre2, password);
+                this.rango = rango2;
+        }
+
+       
+
         
 }
